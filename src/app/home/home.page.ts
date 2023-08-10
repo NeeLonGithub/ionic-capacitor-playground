884960a0e59c09e08eeb3803041b29dbe0208fe1
Corneel Eijsbouts <Corneel.Eijsbouts@ordina.nl>
2023-08-10 14:46:22 +0200

    feat(camera): get photo with camera

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +9,19 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public myImage: string | null = null;
+
   constructor() {}
+
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera
+    });
+
+    this.myImage = image.webPath || null;
+  }
 
 }
